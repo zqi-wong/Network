ef741150ec51aed1e817c258ffe3a1ef881eedf2
Ziqian Wang <2779671262@qq.com>
2020-11-05 20:02:04 +0800

    doing some learning

--- hello.py
+++ hello.py
@@ -1,10 +1,27 @@
 from flask import Flask,request,make_response,redirect,abort,render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    comments = [1,2,3,4]
-    return render_template('index.html',comments=comments)
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',form=form,name=name,current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -15,3 +32,11 @@ def index1():
     response = make_response('<h1>This decument carries a cookie!</h1>')
     response.set_cookie('answer','42')
     return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
